93bc623cdb9557eb264291516626a1b5ece999dd
IgorShchegel <mitosss777@gmail.com>
2023-11-04 22:17:40 +0200

    Result

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,14 @@
-function getElementWidth(content, padding, border) {
-    const totalPading = Number.parseFloat(padding) * 2;
-    const totalBorder = Number.parseFloat(border) * 2;
-    return  Number.parseFloat(content) + totalPading + totalBorder ;
-};
-
-
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+function checkForSpam(message){
+    const result = message.toLowerCase().includes("spam")||message.toLowerCase().includes("sale");
+    return result;
+}
   
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
